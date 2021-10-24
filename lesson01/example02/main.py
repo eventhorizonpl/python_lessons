2dc1d1c09f11634ae0d830f3f94a7a65acc3e532
Michal Piotrowski <michal@eventhorizonlabs.eu>
2021-10-24 09:23:24 +0000

    lesson01 example02

---
+++ lesson01/example02/main.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    print('Give your first name:')
+    first_name = input()
+    print('Give your last name:')
+    last_name = input()
+    print('Give your birth date:')
+    birth_date = input()
+    print('Your name is', first_name, last_name, 'and your birth date is', birth_date)
