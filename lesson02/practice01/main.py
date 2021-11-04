c3b498555cadc685d9643a26d31e5feab0de4686
Michal Piotrowski <michal@eventhorizonlabs.eu>
2021-11-04 15:34:42 +0000

    Rectangle -> Square

--- lesson02/practice01/main.py
+++ lesson02/practice01/main.py
@@ -1,16 +1,16 @@
-class Rectangle:
+class Square:
     a = 0
 
     def __init__(self, a):
         self.a = a
 
     def calculate_the_area(self):
-        print('Area of rectangle is', (self.a * self.a))
+        print('Area of square is', (self.a * self.a))
 
     def calculate_the_circumference(self):
-        print('Circumference of rectangle is', (4 * self.a))
+        print('Circumference of square is', (4 * self.a))
 
 if __name__ == '__main__':
-    rectangle = Rectangle(5)
-    rectangle.calculate_the_area()
-    rectangle.calculate_the_circumference()
+    square = Square(5)
+    square.calculate_the_area()
+    square.calculate_the_circumference()
